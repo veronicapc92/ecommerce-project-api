8dbef69113d6ff201b9ea84ed0b1bd1b25170513
Veronica <veronicapc1992@gmail.com>
2020-04-24 16:56:52 +0200

    fixed errors in user.js and users.js

--- models/user.js
+++ models/user.js
@@ -21,7 +21,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  const token = jwt.sign(
+    { _id: this._id, name: this.name },
+    config.get("jwtPrivateKey")
+  );
   return token;
 };
 
