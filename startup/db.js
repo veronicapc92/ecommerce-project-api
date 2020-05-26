f623f6868bf0994a2c1ea5d99bea495781238ecc
Veronica <veronicapc1992@gmail.com>
2020-05-26 13:06:27 +0200

    updating project

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,12 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
-  mongoose
-    .connect("mongodb://localhost/gosha", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
-    .then(() => winston.info("Connected to MongoDB..."));
+  const db = config.get("db");
+  // .connect("mongodb://localhost/gosha", {
+  //   useUnifiedTopology: true,
+  //   useNewUrlParser: true,
+  // })
+  mongoose.connect(db).then(() => winston.info("Connected to MongoDB..."));
 };
